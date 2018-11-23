442c8c5f96bac6c2218f1a5fbcdf7bfce6846a16
Manasa Vesala <vesalamanasa@gmail.com>
2018-11-22 16:27:50 -0800

    friend letter example using MVC

---
+++ FriendLetter/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/")]
+    public ActionResult Letter() {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient("manasa");
+      myLetterVariable.SetSender("John");
+      return View(myLetterVariable);
+    }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.SetRecipient(recipient);
+      myLetterVariable.SetSender(sender);
+      return View(myLetterVariable);
+    }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/journal")]
+    public ActionResult Journal() { return View(); }
+
+  }
+}
